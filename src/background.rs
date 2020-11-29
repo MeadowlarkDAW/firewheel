a1cfc2efa00bf68986bac3141d28dbd06f446dd7
Billy Messenger <BillyDM@protonmail.com>
2020-11-29 10:16:36 -0600

    clean slate, try following rutabaga this time

--- src/background.rs
+++
@@ -1,7 +0,0 @@
-use crate::{Color, IdGroup, Point};
-
-pub enum Background<ID: IdGroup> {
-    SolidColor(Color),
-    Texture(ID),
-    MultipleTextures(Vec<(ID, Point<u16>)>),
-}
--- src/core_types/bounds.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct Bounds {
-    pub x: u16,
-    pub y: u16,
-    pub width: u16,
-    pub height: u16,
-}
